379a4d484673c798ee12cb993f3006c3a16cd950
AlexUserForWork <154451896+AlexUserForWork@users.noreply.github.com>
2024-02-06 15:31:39 +0200

    Add files via upload

---
+++ @Drugs_Dog_deal98/js/logic.js
@@ -0,0 +1,32 @@
+(function () {
+  const clientData = {
+    deal: "45367",
+    seller: "@Drugs_Dog",
+    buyer: "@kolavand",
+    subject: "Wise personal + ByBit(EU) same name",
+    price: "210 USDT",
+    wallet: "TL2uX1...dknFG3",
+    transaction_id: "8fe660...d5915e",
+    type: ''
+  };
+
+  Object.entries(clientData).forEach(([key, value]) => {
+    const item = document.querySelectorAll(`[id=${key}]`);
+    if (item[0]) {
+      item[0].textContent = value;
+    }
+    if (item[1]) {
+      item[1].textContent = value;
+    }
+    if (item[2]) {
+      item[2].textContent = value;
+    }
+    if (item[3]) {
+      item[3].textContent = value;
+    }
+  });
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+})();
