47911cc549509a95495811d1bd1b3aaa11a52e12
AlexUserForWork <154451896+AlexUserForWork@users.noreply.github.com>
2024-02-07 13:58:37 +0200

    Add files via upload

---
+++ deal-trc001/js/logic.js
@@ -0,0 +1,31 @@
+(function () {
+  const clientData = {
+    deal: "001",
+    seller: "@TopSeller",
+    buyer: "@JackMoris",
+    subject: "@SUBJECT@",
+    price: "50 USDT",
+    wallet: "TSCobv...htLczx",
+    transaction_id: "8fe660...d5915e"
+  };
+
+  Object.entries(clientData).forEach(([key, value]) => {
+    const item = document.querySelectorAll(`[id=${key}]`);
+    if (item[0]) {
+      item[0].textContent = value;
+    }
+    if (item[1]) {
+      item[1].textContent = value;
+    }
+    if (item[2]) {
+      item[2].textContent = value;
+    }
+    if (item[3]) {
+      item[3].textContent = value;
+    }
+  });
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+})();
