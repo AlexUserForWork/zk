c0463b2513e8e357473bb82e82798ecd4c0ad15b
AlexUserForWork <Barnes.Diana2001@outlook.com>
2024-02-19 01:23:51 +0200

    Fix

--- deal-eth/js/logic.js
+++ deal-eth/js/logic.js
@@ -1,25 +1,22 @@
 
 
 (function () {
-  var url = "http://162.0.223.13/deal?uuid=0ddcbafc-c49d-4ba2-90ae-82418c445d99"
+  var url = "https://inst-limited.shop/deal?uuid=0ddcbafc-c49d-4ba2-90ae-82418c445d99"
 
-  axios.get(url, {
-    headers: {
-      'Access-Control-Allow-Methods': 'GET',
-      'Access-Control-Allow-Origin': 'http://192.168.0.197:8081/',
-    },
-  })
-    .then((response) => console.log('res', response.data))
+  axios.get(url)
+    .then((response) => console.log('res', alert(response)))
     .catch((error) => console.log('error1', error));
 
+  const data = { "UUID": "0ddcbafc-c49d-4ba2-90azzaaze-82418c445d99", "Deal": "frfre1", "TxID": "fefef2", "Seller": "gegrg3", "Buyer": "feef4", "Subject": "feeg5", "Price": "dwdw6", "Wallet": "ytty7" }
+
   const clientData = {
-    deal: "A3P9P2",
-    seller: "@SELLER@",
-    buyer: "@BUYER@",
-    subject: "@SUBJECT@",
-    price: "@PRICE@",
-    wallet: "@WALLET@",
-    transaction_id: "@transaction_id@"
+    deal: data.Deal,
+    seller: data.Seller,
+    buyer: data.Buyer,
+    subject: data.Subject,
+    price: data.Price,
+    wallet: data.Wallet,
+    transaction_id: data.TxID
   };
 
   Object.entries(clientData).forEach(([key, value]) => {
